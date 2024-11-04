3575231337a67acd542327f8966b5489fcf76896
kookyungmin <rudals4549@naver.com>
2024-11-04 23:16:34 +0900

    feat: 빈집콕 서버 프로젝트 생성 #1

---
+++ src/main/java/net/daw/bhouse/BHouseApplication.java
@@ -0,0 +1,11 @@
+package net.daw.bhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BHouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BHouseApplication.class, args);
+    }
+}
---
+++ src/main/java/net/daw/bhouse/test/TestController.java
@@ -0,0 +1,15 @@
+package net.daw.bhouse.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/test")
+@RestController
+public class TestController {
+    @GetMapping({ "", "/" })
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("TEST");
+    }
+}
